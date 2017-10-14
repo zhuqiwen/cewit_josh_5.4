788c72f3938175ccb12bb0b7ada7a09b7d30d4d9
Qiwen Zhu <qiwzhu@indiana.edu>
2017-10-14 11:47:11 -0400

    modify title template

--- resources/views/admin/layouts/default.blade.php
+++ resources/views/admin/layouts/default.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <title>
         @section('title')
-            | Josh Admin Template
+            | CEWiT IIS
         @show
     </title>
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
