f09d713763794a4eb51c74dc9e881be723544e6c
Qiwen Zhu <qiwzhu@indiana.edu>
2018-01-18 22:08:28 -0500

    comment out import limitation

--- public/assets/js/data_import/no_access_modal.js
+++ public/assets/js/data_import/no_access_modal.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
-    console.log('ready');
-    $('#no-access-import-data').modal('show');
+    // console.log('ready');
+    // $('#no-access-import-data').modal('show');
 });
 
 
-$('#no-access-import-data').on('hidden.bs.modal', function (e) {
-    window.history.back();
-})
+// $('#no-access-import-data').on('hidden.bs.modal', function (e) {
+//     window.history.back();
+// });
